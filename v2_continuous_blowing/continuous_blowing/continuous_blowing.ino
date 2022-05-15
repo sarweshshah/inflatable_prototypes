17af839cc89c35ad3b103149dee82c3a5da43f9e
Sarwesh Shah <sarweshshah@yahoo.com>
2022-05-15 22:09:31 +0530

    Minor changes

--- v2_continuous_blowing/continuous_blowing/continuous_blowing.ino
+++ v2_continuous_blowing/continuous_blowing/continuous_blowing.ino
@@ -4,5 +4,7 @@ void setup() {
 }
 
 void loop() {
+  
+  // Continous blowing of air pump (no pulse)
   analogWrite(pump, 255);
 }
--- v3_interval_blowing/interval_blowing/interval_blowing.ino
+++ v3_interval_blowing/interval_blowing/interval_blowing.ino
@@ -5,11 +5,14 @@ void setup() {
 }
 
 void loop() {
+  
+  // HIGH signal pulse ratio increasing
   for (int i = 10; i < 255; i+= 1) {
     analogWrite(pump, i);
     delayMicroseconds(2000);
   }
 
+  // HIGH signal pulse ratio decreasing
   for (int i = 255; i > 10; i-= 1) {
     analogWrite(pump, i);
     delayMicroseconds(2000);
