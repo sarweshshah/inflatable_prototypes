74c6f91fac8cba9fa875cee6dbefe3e87e9f3e48
Sarwesh Shah <sarweshshah@yahoo.com>
2022-05-21 11:36:28 +0530

    Adding code for vacuum pump

--- v3_pressure_sensing/pressure_sensing/pressure_sensing.ino
+++ v3_pressure_sensing/pressure_sensing/pressure_sensing.ino
@@ -1,6 +1,7 @@
 #include "HX710B.h"
 
-const int PUMP_PIN = A1;
+const int PUMP_PIN = A0;
+const int VACUUM_PIN = A1;
 const int DOUT_PIN = A3;
 const int SCLK_PIN = A2;
 
@@ -8,19 +9,17 @@ HX710B pressure_sensor;
 
 void setup() {
   Serial.begin(57600);
+
+  // Preparing Pressure Sensor
   pressure_sensor.begin(DOUT_PIN, SCLK_PIN);
   pressure_sensor.power_up();
-
-  // Waiting for 2 secs for the sensor to read the pressure and send signal
-  pressure_sensor.wait_ready(2000);
+  pressure_sensor.wait_ready(2000); // Waiting for 2 secs for the sensor to send signal
   Serial.println("HX710B Sensor is ready!");
 }
 
 void loop() {
-  // Sending half-pulse HIGH signal to the air pump
-  //    analogWrite(PUMP_PIN, 128);
-  // for full pulse HIGH signal
   analogWrite(PUMP_PIN, 255);
+  analogWrite(VACUUM_PIN, 128); // Half-intensity
 
   if (pressure_sensor.is_ready()) {
     Serial.print(pressure_sensor.pascal() / 1000);
